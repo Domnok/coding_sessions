cc586848f0db2c200713e2157bb0ad4e4a7ee6eb
Domnok <domnok@Domn0kPC.localdomain>
2021-01-31 11:55:36 -0500

    payroll app updated w/ functional modularity a trimmed

---
+++ templates/simple_project/README.md
@@ -0,0 +1,9 @@
+
+Project Title
+-Payroll App
+
+Project Date
+
+
+Project Description
+-Calculates pay based on user input.
--- templates/simple_project/main/3.9_Payroll_Lang.py
+++ templates/simple_project/main/3.9_Payroll_Lang.py
@@ -1,93 +1,103 @@
 
-# Input
-'''Get Hours worked, Number of Hours, Pay Rate, State/Fed Tax Witholding'''
+'''This function takes user input as the number of hours worked and returns that value'''
 
-# Hours must be int
+def get_hours():
+	try: 
+		hours = float(input("Number of Hours: "))
 
-try: 
-	hours = float(input("Number of Hours: "))
+	except ValueError as err:
+		print("INFO: Hours must be a number")
+		print('DEBUG: {0}'.format(err))
+		exit()
+	except Exception as err:
+		print("Oops!, something went wrong.")
+		print('DEBUG: {0}'.format(err))
+		exit() 
 
-except ValueError as err:
-	print("INFO: Hours must be a number")
-	print('DEBUG: {0}'.format(err))
-	exit()
-except Exception as err:
-	print("Oops!, something went wrong.")
-	print('DEBUG: {0}'.format(err))
-	exit() 
+	return hours
+
+'''This function takes user input as the dollar ammount paid hourly and returns that value'''
+
+def get_pay_rate():
+	try:
+		pay_rate = float(input("Dollars per hour: "))
+
+	except ValueError as err:
+		print("INFO: Dollars per hour must be a number.")
+		print('DEBUG: {0}'.format(err))
+		exit()
+	except Exception as err:
+		print("Oops!, something went wrong.")
+		print('DEBUG: {0}'.format(err))
+		exit()
+	return pay_rate
+	
+'''This function takes user input as federal tax as a percentage(excluding the "%")'''
+
+def get_fed_tax_pcnt():
+	try:
+		fed_tax_percent = float(input("Enter Federal Tax Rate As %: "))
+		fed_tax = fed_tax_percent / 100
+
+	except ValueError as err:
+		print("INFO: Federal Tax Rate Must be a number greater than 0.")
+		print('DEBUG: {0}'.format(err))
+		exit()
+	except Exception as err:
+		print("Oops!, something went wrong.")
+		print('DEBUG: {0}'.format(err))
+		exit()
+
+	return fed_tax
+
+'''This function takes user input as state tax as a percentage(excluding the "%")'''
+
+def get_state_tax_pcnt():
+	try:
+		state_tax_percent = float(input("Enter State Tax Rate As %: "))
+		state_tax = state_tax_percent / 100
+
+	except ValueError as err:
+		print("INFO: State Tax Rate Must be a number greater than 0.")
+		print('DEBUG: {0}'.format(err))
+		exit()
+	except Exception as err:
+		print("Oops!, something went wrong.")
+		print('DEBUG: {0}'.format(err))
+		exit()
+	return state_tax
+
+'''This function calculates grosspay by multiplying hours(input) by hourly pay rate(input)'''
+# def calc_gross_pay(hours, pay_rate):
+def calc_gross_pay():
+	gross_pay = get_hours() * get_pay_rate()
+	# gross_pay = hours * pay_rate
+	return gross_pay
+
+'''This function calculates tax bill by multiplying grosspay(input) by fed and state tax rate combined value'''
+# def calc_tax_bill(gross_pay, total_tax_rate):
+def calc_tax_bill(gross_pay):
+	tax_bill = gross_pay * (get_fed_tax_pcnt() + get_state_tax_pcnt())
+	# tax_bill = gross_pay * total_tax_rate
+	return tax_bill
+
+'''This function calculates net pay by deducting the tax bill value from grosspay value'''
+def calc_net_pay(gross_pay, tax_bill):
+	# net_pay = calc_gross_pay() - calc_tax_bill()
+	net_pay = gross_pay - tax_bill
+	return net_pay
+
+if __name__ == "__main__":
+
+	gross_pay = calc_gross_pay()
+
+	tax_bill = calc_tax_bill(gross_pay)
+	
+	net_pay = calc_net_pay(gross_pay, tax_bill)
 
 
+	print('Gross Pay: ${0:.2f}'.format(gross_pay))
 
-# Pay
-try:
-	pay_rate = float(input("Dollars per hour: "))
-except ValueError as err:
-	print("INFO: Dollars per hour must be a number.")
-	print('DEBUG: {0}'.format(err))
-	exit()
-except Exception as err:
-	print("Oops!, something went wrong.")
-	print('DEBUG: {0}'.format(err))
-	exit()
+	print('Tax Bill: ${0:.2f}'.format(tax_bill))
 
-# Federal tax withholding must be float
-try:
-	fed_tax_percent = float(input("Enter Federal Tax Rate As %: "))
-	fed_tax = fed_tax_percent / 100
-except ValueError as err:
-	print("INFO: Federal Tax Rate Must be a number greater than 0.")
-	print('DEBUG: {0}'.format(err))
-	exit()
-except Exception as err:
-	print("Oops!, something went wrong.")
-	print('DEBUG: {0}'.format(err))
-	exit()
-
-
-
-
-
-# State tax withholding must be float
-
-try:
-	state_tax_percent = float(input("Enter State Tax Rate As %: "))
-	state_tax = state_tax_percent / 100
-except ValueError as err:
-	print("INFO: State Tax Rate Must be a number greater than 0.")
-	print('DEBUG: {0}'.format(err))
-	exit()
-except Exception as err:
-	print("Oops!, something went wrong.")
-	print('DEBUG: {0}'.format(err))
-	exit()
-
-# Compute
-
-# NetPay = (pay_rate * Hours) * (fed_tax + state_tax)
-
-# Gross pay: Hours * Pay
-
-gross_pay = hours * pay_rate
-
-# TaxBill: gross_pay * (fed_tax + state_tax)
-
-tax_bill = gross_pay * (fed_tax + state_tax)
-
-# NetPay: gross_pay - tax_bill
-
-net_pay = gross_pay - tax_bill
-
-
-# Output
-
-print('Hours: {0}Hrs'.format(hours))
-
-print('Pay: {0:.2f}/Hr'.format(pay_rate))
-
-print('Gross Pay: ${0:.2f}'.format(gross_pay))
-
-print('Tax Bill: ${0:.2f}'.format(tax_bill))
-
-print('Net Pay: ${0:.2f}'.format(net_pay))
-# Error
-'''When entering numbers with spaces error: "cd: string not in pwd: -5" '''
+	print('Net Pay: ${0:.2f}'.format(net_pay))
